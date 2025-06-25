f806253786120d05e4b4b7e51421d9001721045f
zach2017 <zaclewis@gmail.com>
2025-06-25 12:06:46 -0500

    add icon map

--- mapdemo/src/App.tsx
+++ mapdemo/src/App.tsx
@@ -1,17 +1,24 @@
 
 import './App.css'
+import type { PointData } from './MapComponent';
 import MapContainer from './MapContainer';
 
 const App: React.FC = () => {
   const location = {
-    latitude: 51.505,
-    longitude: -0.09,
+    latitude: 32.3800,
+    longitude: -86.3100,
     zoom: 13,
   };
 
+  const examplePoints : PointData[] = [
+    { latitude: 32.3792, longitude: -86.3077, iconSize: 64 },
+    { latitude: 32.3800, longitude: -86.3100, iconSize: 32 },
+    { latitude: 32.3810, longitude: -86.3120, iconSize: 16 },
+  ];
+
   return (
     <div>
-      <MapContainer location={location} />
+      <MapContainer location={location} points={examplePoints} />
     </div>
   );
 };
--- mapdemo/src/MapContainer.tsx
+++ mapdemo/src/MapContainer.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Collapse, Box, Typography } from "@mui/material";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import MapComponent from "./MapComponent";
+import MapComponent, { type PointData } from "./MapComponent";
 
 interface Location {
   latitude: number;
@@ -11,9 +11,19 @@ interface Location {
 
 interface MapContainerProps {
   location: Location;
+  points: PointData[];
 }
 
-const MapContainer: React.FC<MapContainerProps> = ({ location }) => {
+const examplePoints: PointData[] = [
+  { latitude: 32.3792, longitude: -86.3077, iconSize: 64 },
+  { latitude: 32.38, longitude: -86.31, iconSize: 32 },
+  { latitude: 32.381, longitude: -86.312, iconSize: 16 },
+];
+
+const MapContainer: React.FC<MapContainerProps> = ({
+  location,
+  points = { examplePoints },
+}) => {
   const [expanded, setExpanded] = useState<boolean>(false);
 
   const handleToggle = () => {
@@ -54,7 +64,7 @@ const MapContainer: React.FC<MapContainerProps> = ({ location }) => {
         />
       </Box>
       <Collapse in={expanded} timeout="auto">
-        <MapComponent location={location} />
+        <MapComponent location={location} points={points} />
       </Collapse>
     </Box>
   );
